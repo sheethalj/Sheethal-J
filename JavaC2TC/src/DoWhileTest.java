f7348d9f519929e732edc4aa38a54f10a09752c3
Madhu <Madhu@DESKTOP-MC0JPU5>
2022-05-25 18:54:43 +0530

    Do while test

---
+++ JavaC2TC/src/DoWhileTest.java
@@ -0,0 +1,16 @@
+
+
+public class DoWhileTest  
+{ 
+public static void main(String args[]) 
+{ 
+int i = 20; 
+do 
+{ 
+System.out.println(i); 
+i = i+1; 
+} while (i <= 20); 
+
+System.out.println(i);
+} 
+}
