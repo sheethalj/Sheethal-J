927852eb382a03fd48ad3827550a1f0d5d9810ad
Madhu <Madhu@DESKTOP-MC0JPU5>
2022-06-20 15:55:09 +0530

    First commit

---
+++ TNSIF/src/TNSIF/Testtrycatch1.java
@@ -0,0 +1,19 @@
+package TNSIF;
+
+import java.util.Scanner;
+public class Testtrycatch1 {
+	public static void main(String args[]) {
+		try {
+			Scanner sc = new Scanner(System.in);
+			int a, b;
+			System.out.println("enter the value of a and b");
+			a = sc.nextInt();
+			b = sc.nextInt();
+			int data = a / b; // may throw exception
+			System.out.println("data value is " + data);
+		} catch (ArithmeticException ae) {
+			System.out.println(ae);
+		}
+		System.out.println("rest of the code...");
+	}
+}
